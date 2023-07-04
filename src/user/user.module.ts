3ef059001701013523fbe9dc402afce3c6501c59
npdev <sashko.dev@gmail.com>
2023-07-04 19:35:53 +0100

    user & comments modules

---
+++ src/user/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { CommentService } from "../comment/comment.service";
+
+@Module({
+  controllers: [UserController],
+  providers: [UserService, CommentService]
+})
+
+export class UserModule {}
---
+++ src/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from "./dto/createUserDto";
+
+@Injectable()
+export class UserService {
+  findOne(id: string) {
+    return {
+      id
+    };
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+}
